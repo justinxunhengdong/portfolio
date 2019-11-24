8ec5c3c32898ce0d846ada6d211f80b92adc2d1f
Justin Dong <justin.dong@uq.net.au>
2019-11-25 09:04:39 +1000

    markdown work

--- src/app/content-tileset/content-tileset.component.ts
+++ src/app/content-tileset/content-tileset.component.ts
@@ -1,8 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { Content } from '../../dictionaries/datatypes/content-type';
 import { Filter } from '../../dictionaries/datatypes/filter-type';
 import { CONTENTS } from '../../dictionaries/content-dict';
 import { FILTERS } from '../../dictionaries/filter-dict';
+import { MarkdownModule } from 'ngx-markdown';
+
+@NgModule({
+imports: [
+  MarkdownModule.forRoot()
+]
+})
 
 @Component({
   selector: 'app-content-tileset',
@@ -12,11 +21,30 @@ import { FILTERS } from '../../dictionaries/filter-dict';
 export class ContentTilesetComponent implements OnInit {
 
   currentlySelected: Content;
-
+  contentFiltered: Content[];
+  contentsAll: Content[];
+  filtersUsed: Filter[];
+  shouldShowModal: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.contentsAll = CONTENTS;
+    this.filtersUsed = FILTERS;
+    this.shouldShowModal = false;
   }
+
+  /* Overlay  */
+  selectContent(selectedContent: Content): void {
+    this.shouldShowModal = true;
+    this.currentlySelected = selectedContent;
+    console.log("SWITCH");
+  }
+
+  deselectContent() {
+    this.currentlySelected = null;
+    this.shouldShowModal = false;
+  }
+
 
 }
